c7362f6cfe540110e6ce08007842e8a1af6052e3
Stephan Pitois <spitois@gmail.com>
2022-12-06 18:33:11 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -12,14 +12,12 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-		background: lightyellow
+		background: #fff2cc;
 	}
 
 	h1 {
-		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
