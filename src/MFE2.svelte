67debe82a93efb75631e034ae65cdd35bd47ba83
Stephan Pitois <spitois@gmail.com>
2023-01-31 12:19:50 -0500

    Remove mfeCard

--- src/MFE2.svelte
+++ src/MFE2.svelte
@@ -1,20 +1,10 @@
 <script>
 </script>
 
-<div class="mfeCard wrapper">
-    <div class="mfeCardBody">
-        <main class="mfeMain">
-            <h1 class="mfeMainH1">MFE 2</h1>
-            <p>
-                Component <code>remote_app_2.MFE2</code> shows that several
-                micro-frontends can exist on the same page.
-            </p>
-        </main>
-    </div>
-</div>
-
-<style>
-    .wrapper .mfeCard .mfeCardBody {
-        padding: 10px;
-    }
-</style>
+<main>
+    <h1>MFE 2</h1>
+    <p>
+        This shows that several micro-frontends from different bundles can exist
+        on the same page.
+    </p>
+</main>
